b76f0a6204411156cf5d11bb5ea6ab350e072e6c
Chethan Hebbar <chethan0hebbar@gmail.com>
2021-03-05 13:14:25 +0530

    Initial Front End, Server and Schema added

---
+++ app.js
@@ -0,0 +1,47 @@
+//jshint esversion:6
+
+// getting all required packages
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+
+// setting up express server
+const app = express();
+
+// setting ejs template
+app.set('view engine', 'ejs');
+
+// using body-parser to parse through json
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+// rendering home page from server
+app.get("/", function(req, res){
+  res.render("home");
+});
+
+// rendering search page if requested
+app.get("/search", function(req, res){
+  res.render("search");
+});
+
+// rendering borrow page if requested
+app.get("/borrow", function(req, res){
+  res.render("borrow");
+});
+
+// rendering renew page if requested
+app.get("/renew", function(req, res){
+  res.render("renew");
+});
+
+// rendering return page if requested
+app.get("/return", function(req, res){
+  res.render("return");
+});
+
+// connecting to the server: port 3000
+app.listen(3000, function() {
+  console.log("Server up and running on port 3000");
+});
